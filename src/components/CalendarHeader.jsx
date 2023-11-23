c807d7f95c56967683a0664bec58d95fd4d0f346
Hrithik <slumdog4321@gmail.com>
2023-11-23 18:50:38 +0530

    Calendar header added

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import logo from "/googleCal.png";
 
 function CalendarHeader() {
-  return <div>CalendarHeader</div>;
+  return (
+    <div className="px-4 py-2 flex items-center">
+      <img src={logo} alt="calender" className="mr-2 w-12 h-12" />
+      <h1 className="mr-10 text-xl text-gray-500 fond-bold">Calendar</h1>
+      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button>
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          chevron_left
+        </span>
+      </button>
+      <button>
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          chevron_right
+        </span>
+      </button>
+    </div>
+  );
 }
 
 export default CalendarHeader;
