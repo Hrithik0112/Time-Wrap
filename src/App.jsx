f68415f762f98e0d4df8fba4706934d51db0b2cf
Hrithik <slumdog4321@gmail.com>
2023-11-23 18:04:18 +0530

    initial components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import CalendarHeader from "./components/CalendarHeader";
+import Month from "./components/Month";
+import Sidebar from "./components/Sidebar";
 import { getMonth } from "./utils";
 
 function App() {
   console.table(getMonth(3));
   return (
-    <main>
-      <header className="font-bold text-3xl">Welcome To the Dark Side!!</header>
-    </main>
+    <>
+      <div className="h-screen flex flex-col">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Month.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Month() {
+  return <div>Month</div>;
+}
+
+export default Month;
