ea57403b8b8d8ea302c04d7bf5d2161ad2b5fcbe
Hrithik <slumdog4321@gmail.com>
2023-11-24 18:05:18 +0530

    create event button added in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CreateEventButton from "./CreateEventButton";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="border p-5 w-64">
+      <CreateEventButton />
+    </aside>
+  );
 }
 
 export default Sidebar;
