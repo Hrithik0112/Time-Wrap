a6230fa21f49277dd2f76bc19f3ffcf2cd87db95
Hrithik <slumdog4321@gmail.com>
2023-11-25 11:39:52 +0530

    added small calender , in sync with big one

--- src/context/ContextWrapper.jsx
+++ src/context/ContextWrapper.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalContext from "./GlobalContext";
 import dayjs from "dayjs";
 
 export default function ContextWrapper(props) {
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [smallCalendarMonth, setSmallCalendarMonth] = useState(null);
+  const [daySelected, setDaySelected] = useState(dayjs());
+
+  useEffect(() => {
+    if (smallCalendarMonth !== null) {
+      setMonthIndex(smallCalendarMonth);
+    }
+  }, [smallCalendarMonth]);
   return (
-    <GlobalContext.Provider value={{ monthIndex, setMonthIndex }}>
+    <GlobalContext.Provider
+      value={{
+        monthIndex,
+        setMonthIndex,
+        smallCalendarMonth,
+        setSmallCalendarMonth,
+        daySelected,
+        setDaySelected,
+      }}
+    >
       {props.children}
     </GlobalContext.Provider>
   );
