f021bd5ef3a64464135879a69959e855db2ba613
Hrithik <slumdog4321@gmail.com>
2024-02-01 00:19:00 +0530

    some name fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ...labelsClasses.map((lbl) => `text-${lbl}-400`),
     ],
   },
-  darkMode: false, // or 'media' or 'class'
+  // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
